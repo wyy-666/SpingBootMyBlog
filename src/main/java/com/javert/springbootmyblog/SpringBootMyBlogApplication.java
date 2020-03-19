1fa070fb2b616b0c3ce579dc0a087f7ed7e5896e
junkai.wang <junkai.wang@heweisoft.com>
2020-03-19 14:40:55 +0800

    测试是否正常提交

---
+++ src/main/java/com/javert/springbootmyblog/SpringBootMyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.javert.springbootmyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyBlogApplication.class, args);
+    }
+
+}
